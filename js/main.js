4fa8f0753c38ddbdee585e94432eb80700e8a7f1
Bhuvnish <b_arora163217@fanshaweonline.ca>
2023-04-01 15:33:59 -0400

    completing

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@
 
 let bhuvnish = document.querySelectorAll('.button a'),
     lightBox = document.querySelector('#lightbox'),
-    headline = lightBox.querySelector('#lb_heading'),
-    bio = lightBox.querySelector('#lb_text');
+    headline = lightBox.querySelector('.lb_heading'),
+    bio = lightBox.querySelector('.lb_text');
 ;
 
 
-function loadMyData() {
-    //debugger;
+function loadmyData() {
+    
 
     lightBox.querySelector('h3').textContent = bhuvnish[this.dataset.member].headline;
     lightBox.querySelector('p').textContent = bhuvnish[this.dataset.member].bio;
 
 }
 
-bhuvnish.forEach(hero => hero.addEventListener('click',loadMyData));
+bhuvnish.forEach(hero => hero.addEventListener('click',loadmyData));
 
 
 let audin = document.querySelectorAll('.button a'),
@@ -26,7 +26,7 @@ let audin = document.querySelectorAll('.button a'),
 
 
 function loadMyData() {
-    //debugger;
+   
 
     lightBox.querySelector('h3').textContent = audin[this.dataset.member].headline;
     lightBox.querySelector('p').textContent = audin[this.dataset.member].bio;
